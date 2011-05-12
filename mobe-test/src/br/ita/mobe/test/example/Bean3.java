4afebf570bf2dfcf4bf27789fe42a1ef96c19063
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2011-05-12 19:31:31 -0300

    Added test project.

---
+++ mobe-test/src/br/ita/mobe/test/example/Bean3.java
@@ -0,0 +1,26 @@
+package br.ita.mobe.test.example;
+
+public class Bean3 {
+
+	private String firstName = "Bruno";
+	private String surname;
+	private int myAge = 22;
+	private boolean alive = true;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public int getMyAge() {
+		return myAge;
+	}
+
+	public boolean isAlive() {
+		return alive;
+	}
+
+}
---
+++ mobe-test/src/br/ita/mobe/test/utils/StringUtils.java
@@ -0,0 +1,16 @@
+package br.ita.mobe.test.utils;
+
+public class StringUtils {
+
+	public static boolean isEmpty(String string) {
+		if (string == null) {
+			return true;
+		}
+		String trim = string.trim();
+		if (trim.length() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
