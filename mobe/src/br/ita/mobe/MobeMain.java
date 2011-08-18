33681a369529f50687fce68113eca868c80abc3d
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2011-08-18 00:44:15 -0300

    Small changes.

--- mobe/src/br/ita/mobe/MobeMain.java
+++ mobe/src/br/ita/mobe/MobeMain.java
@@ -50,5 +50,4 @@ public class MobeMain extends Activity {
 			}
 		});
 	}
-
 }
--- mobe/src/br/ita/mobe/pattern/PersistenceController.java
+++ mobe/src/br/ita/mobe/pattern/PersistenceController.java
@@ -1,10 +1,17 @@
 package br.ita.mobe.pattern;
 
 import android.content.Context;
+import br.ita.mobe.pattern.metadata.BeanMetadata;
+import br.ita.mobe.pattern.metadata.Repository;
 
 public class PersistenceController {
 
 	public void save(Object bean, Context context) {
+		BeanMetadata metadata = Repository.getInstance().getMetadata(bean);
+
+		// open (create/update) database
+
+		// save bean
 
 	}
 }
