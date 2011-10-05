31e80810367ce21d11870c3d1d79a728748c8313
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2011-10-05 08:06:11 -0300

    Organized java test suite.

--- mobe-test-java/src/resources/AllTests.java
+++ mobe-test-java/src/resources/AllTests.java
@@ -7,9 +7,10 @@ import org.junit.runners.Suite.SuiteClasses;
 import br.com.mobe.core.metadata.ClassMetadataTest;
 import br.com.mobe.core.metadata.RepositoryTest;
 import br.com.mobe.core.reader.ConcreteMetadataReaderTest;
+import br.com.mobe.orm.db.DatabaseBuilderTest;
 
 @RunWith(Suite.class)
-@SuiteClasses(value = { ClassMetadataTest.class, RepositoryTest.class, ConcreteMetadataReaderTest.class })
+@SuiteClasses(value = { ClassMetadataTest.class, RepositoryTest.class, ConcreteMetadataReaderTest.class, DatabaseBuilderTest.class })
 public class AllTests {
 
 }
