d3dcb429266152d5f028bc442d89d70d08c78512
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2011-10-31 14:42:14 -0200

    Refactoring view population.

--- mobe/src/br/com/mobe/view/logic/layer/PopulateFormLayer.java
+++ mobe/src/br/com/mobe/view/logic/layer/PopulateFormLayer.java
@@ -1,49 +1,25 @@
 package br.com.mobe.view.logic.layer;
 
-import java.lang.reflect.Field;
 import java.util.List;
 
 import android.content.Context;
-import android.util.Log;
 import br.com.mobe.core.metadata.ClassMetadata;
 import br.com.mobe.core.metadata.Property;
 import br.com.mobe.view.FormView;
+import br.com.mobe.view.logic.process.ViewProcessor;
 import br.com.mobe.view.widget.form.FormWidget;
 
 public class PopulateFormLayer implements FormProcessingLayer {
 
-	private static final String TAG = "PopulateFormLayer";
-
 	@Override
 	public FormView processLayer(Context context, FormView formView, ClassMetadata metadata, Object bean) {
-		// TODO: como integrar com o Metadata Processor?
-
-		Class<?> clazz = bean.getClass();
 		List<Property> properties = metadata.getProperties();
 		for (Property property : properties) {
-			String propertyName = property.getName();
-			FormWidget widget = formView.findWidget(propertyName);
-			Object value = null;
-			try {
-				Field field = clazz.getDeclaredField(propertyName);
-				field.setAccessible(true);
-				value = field.get(bean);
-			} catch (SecurityException e) {
-				Log.e(TAG, "", e);
-				e.printStackTrace();
-			} catch (NoSuchFieldException e) {
-				Log.e(TAG, "", e);
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				Log.e(TAG, "", e);
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				Log.e(TAG, "", e);
-				e.printStackTrace();
-			}
-			widget.populate(value);
+			ViewProcessor processor = property.getViewProcessor();
+			String name = property.getName();
+			FormWidget widget = formView.findWidget(name);
+			processor.populateWidget(widget, name, bean);
 		}
 		return formView;
 	}
-
 }
--- mobe/src/br/com/mobe/view/logic/process/ViewProcessor.java
+++ mobe/src/br/com/mobe/view/logic/process/ViewProcessor.java
@@ -1,11 +1,39 @@
 package br.com.mobe.view.logic.process;
 
+import java.lang.reflect.Field;
+
 import android.content.Context;
+import android.util.Log;
 import br.com.mobe.view.widget.form.FormWidget;
 
 public abstract class ViewProcessor {
 
+	private static final String TAG = "ViewProcessor";
+
 	public abstract FormWidget createWidget(Context context, String name);
+
+	public void populateWidget(FormWidget widget, String name, Object bean) {
+		Class<?> clazz = bean.getClass();
+		Object value = null;
+		try {
+			Field field = clazz.getDeclaredField(name);
+			field.setAccessible(true);
+			value = field.get(bean);
+		} catch (SecurityException e) {
+			Log.e(TAG, "", e);
+			e.printStackTrace();
+		} catch (NoSuchFieldException e) {
+			Log.e(TAG, "", e);
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			Log.e(TAG, "", e);
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			Log.e(TAG, "", e);
+			e.printStackTrace();
+		}
+		widget.populate(value);
+	}
 
 	protected String capitalize(String name) {
 		StringBuilder sb = new StringBuilder();
