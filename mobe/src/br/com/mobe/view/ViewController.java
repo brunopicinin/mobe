e1c05f90fbe4b126e481e0b85a54cbdfa4f6da6c
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2011-11-01 01:45:28 -0200

    Some documentations added.

--- mobe/src/br/com/mobe/view/ViewController.java
+++ mobe/src/br/com/mobe/view/ViewController.java
@@ -15,11 +15,27 @@ public class ViewController {
 		this.context = context;
 	}
 
+	/**
+	 * Generate a form based on a Java Bean.
+	 * 
+	 * @param bean
+	 *            The object that will serve as model to the form.
+	 * @return The generated form.
+	 * @throws IllegalMetadataException
+	 */
 	public FormView generateForm(Object bean) throws IllegalMetadataException {
 		ClassMetadata metadata = Repository.getInstance().getMetadata(bean.getClass());
 		return new GenerateFormLayer().processLayer(context, null, metadata, null);
 	}
 
+	/**
+	 * Generate and populate a form based on a Java Bean.
+	 * 
+	 * @param bean
+	 *            The object that will serve as model to the form and that will be used to populate the form with its field values.
+	 * @return The generated and populated form.
+	 * @throws IllegalMetadataException
+	 */
 	public FormView generatePopulateForm(Object bean) throws IllegalMetadataException {
 		ClassMetadata metadata = Repository.getInstance().getMetadata(bean.getClass());
 		FormView form = new GenerateFormLayer().processLayer(context, null, metadata, null);
